a863c3e2b30be37da6427ad447ea8340acda0e81
Donoras <donoras@gmail.com>
2021-03-09 10:43:07 +0200

    Pridėjau contact_us.html

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField
+from wtforms import StringField, TextAreaField, SubmitField, PasswordField, SelectField, BooleanField
 from wtforms.validators import DataRequired, Length, Email
 
 
